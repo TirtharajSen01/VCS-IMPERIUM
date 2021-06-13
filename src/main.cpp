36bda3ab054f864a1e893df61b0349db694b6aeb
TirtharajSen01 <sentirtharaj01@gmail.com>
2021-06-13 20:33:57 +0530

    imperium add

--- src/main.cpp
+++ src/main.cpp
@@ -50,13 +50,167 @@ void init(string path)
     }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// IMPERIUM ADD FUNCTION
+
+void add(string path);
+
+string getCurrentPath(string path)
+{
+    int len = root.length();
+    if (path.substr(0, len) == root)
+        return path.substr(len + 1);
+    return path;
+}
+
+void addToLog(string path)
+{
+    ifstream fin((root + "/.imperium/add.log").c_str());
+    string check;
+    while (getline(fin, check)) {
+        if (check == ("/" + path))
+            return;
+    }
+    fin.close();
+    ofstream fout((root + "/.imperium/add.log").c_str(), fstream::app);
+    fout << "/" + path << endl;
+    fout.close();
+}
+
+void addToCache(string path, char dir_type)
+{
+    struct stat info;
+    if (checkDirType(root + "/.imperium/.add") != "directory")
+        mkdir((root + "/.imperium/.add").c_str(), 0777);
+
+    if (dir_type == 'f') {
+        string filePath = root + "/.imperium/.add/" + getCurrentPath(path);
+        const fs::path file_path = filePath;
+        fs::create_directories(file_path.parent_path());
+        fs::copy_file(path, filePath, fs::copy_options::update_existing);
+    }
+    else if (dir_type == 'd') {
+        string filePath = root + "/.imperium/.add/" + getCurrentPath(path);
+        struct stat infofile;
+        const fs::path file_path = filePath;
+        if (checkDirType(filePath) != "directory")
+            fs::create_directories(file_path);
+    }
+}
+
+bool ignoreFolder(string path, vector<string> dirs)
+{
+    for (auto currdir : dirs) {
+        currdir.pop_back();
+        if (currdir.find(path) != string::npos)
+            return true;
+    }
+    return false;
+}
+
+bool toBeIgnored(string path, int onlyImperiumIgnore = 1)
+{
+    ifstream addFile((root + "/.imperium/add.log").c_str());
+    ifstream ignoreFile((root + "/.imperiumignore").c_str());
+    string check;
+    vector<string> filenames;
+    vector<pair<string, char>> addedFileNames;
+    vector<string> ignoreDir;
+
+    path = getCurrentPath(path);
+    while (getline(ignoreFile, check)){
+        if (check == "/" + path.substr(0, check.size() - 1))
+            return true;
+    }
+    ignoreFile.close();
+    addFile.close();
+    return false;
+
+}
+
+void add(string path)
+{
+    struct stat info;
+    if (stat((root + "/.imperium").c_str(), &info) == 0) {
+        struct stat infopath;
+        if (stat(path.c_str(), &infopath) == 0) {
+            if (infopath.st_mode & S_IFDIR) // Checking if the path's of a directory
+            {
+                if (toBeIgnored(path))
+                    return;
+
+                addToLog(path);
+                addToCache(path, 'd');
+                cout << "Added Directory : " << "\"" << path << "\"" << endl;
+
+                for (auto &childdir : fs::recursive_directory_iterator(path)) {
+                    if (toBeIgnored(childdir.path()))
+                        continue;
+                    struct stat infochild;
+                    if (stat(childdir.path().c_str(), &infochild) == 0) {
+                        if (infochild.st_mode & S_IFDIR)    // Checking if the child path's of a directory
+                        {
+                            addToLog(childdir.path());
+                            addToCache(childdir.path(), 'd');
+                            cout << "Added Directory : " << childdir.path() << endl;
+                        }
+                        else if (infochild.st_mode & S_IFREG)   // Checking if the child path's of a file
+                        {
+                            addToLog(childdir.path());
+                            addToCache(childdir.path(), 'f');
+                            cout << "Added File : " << childdir.path() << endl;
+                        }
+                    }
+                }
+            }
+            else if (infopath.st_mode & S_IFREG)    // Checking if the path's of a file
+            {
+                if (toBeIgnored(path))
+                    return;
+
+                addToLog(path);
+                addToCache(path, 'f');
+                cout << "Added File : " << "\"" << path << "\"" << endl;
+            }
+            else
+            {
+                cout << "ERROR. Invalid Path.\n";
+            }
+
+        }
+    }
+    else {
+        cout << "Repository has not been initialized yet.\n";
+    }
+}
+
+
 int main(int argc, char** argv){
 
     const string dir = getenv("dir");
     root = dir;
 
-    if (strcmp(argv[1], "init") == 0){
-        init(root);
+    if (strcmp(argv[1], "init") == 0) {
+        if (argc == 2) {
+            init(root);
+        }
+        else {
+            cout << "ERROR. Command arguments isn't valid.\n";
+            cout << "Did you mean the command \"imperium init\".\n";
+        }
+    }
+    else if (strcmp(argv[1], "add") == 0) {
+        if (argc == 3) {
+            string path = root;
+            path += "/";
+            if (strcmp(argv[2], ".") != 0)
+                path += argv[2];
+            add(path);
+        }
+        else {
+            cout << "Please specify a proper path.\n";
+        }
     }
 
     return 0;
